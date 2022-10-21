e358f4b145fc7e070fea80bedbee1be4147d80db
Steven Seley Jr <seleysteven2@gmail.com>
2022-10-20 19:34:03 -0500

    hotfix-api server error

--- server/index.js
+++ server/index.js
@@ -24,11 +24,10 @@ app.use('/post', postRoutes);
 // Database connecion 
 // TODO: PLEASE MAKE SURE YOU MOVE THIS TO A GIT IGNORED ENV FILE BEFORE DEPLOYMENT
 const CONNECTION_URL = 'mongodb+srv://seley:SeleyK@cluster0.a3dmwyh.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT|| 5000;
 
 // used for avoiding errors in the console
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUNifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
     .catch((error) => console.log(error.message));
-
-
+    
