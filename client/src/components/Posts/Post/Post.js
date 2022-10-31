b0d768d225e1393ddedcb760f65b63ca3345e68f
Steven Seley Jr <seleysteven2@gmail.com>
2022-10-30 21:17:41 -0500

    added like feature

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import moment from 'moment';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 
 const Post = ({ post, setCurrentId }) => {
@@ -35,10 +35,10 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component='p' gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
                     Likes:
                     {post.likeCount}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,12 +3,13 @@ import express from 'express';
 // gives the ability to route
 const router = express.Router();
 // import controller
-import { getPost, createPost, updatePost, deletePost } from '../controller/post.js'
+import { getPost, createPost, updatePost, deletePost, likePost } from '../controller/post.js'
 
 // routes
 router.get('/', getPost);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
